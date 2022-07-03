5ff3539d389348b567aacb043e290d0d991d4c4f
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-03 21:42:24 +0300

    Add files via upload

---
+++ Course_Js_Task4/js/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+
+const operator = prompt('Enter operator ( add, sub, mult, div )');
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+if (operator == '-') {
+    result = number1 - number2;
+}
+if (operator == '*') {
+    result = number1 * number2;
+}
+if (operator == '/'){
+    result = number1 / number2;
+}
+
+alert(`${number1} ${operator} ${number2} = ${result}`);
